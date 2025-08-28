501f4b4bb1c6d5136659243935d53e83791da14a
Belema_adim <adimbelema205@gmail.com>
2025-08-28 11:54:58 +0100

    creating the views in views.py

--- ecolog_api_project/Ecolog/topics/views.py
+++ ecolog_api_project/Ecolog/topics/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Topic
+from .serializers import TopicSerializer
+
+class TopicListView(generics.ListAPIView):
+    queryset = Topic.objects.all()
+    serializer_class = TopicSerializer
 
 # Create your views here.
