c8dd41e4afea244d4ef8e3a7490814e0db9cc8d7
Belema_adim <adimbelema205@gmail.com>
2025-08-31 21:16:36 +0100

    adjusting gunicorn

--- ecolog_api_project/Ecolog/manage.py
+++ ecolog_api_project/Ecolog/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Ecolog.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecolog_api_project.Ecolog.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
